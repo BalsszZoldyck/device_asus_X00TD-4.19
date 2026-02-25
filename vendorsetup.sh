131e5b54d1649ab8b9719e1af6414e3d895fdd22
Tiktodz <dotkit@electrowizard.me>
2026-02-25 15:19:18 +0000

    X00TD: Customify
    Change-Id: Id05cd273a5ed4bbf067c404894c9a79e5be70805
    Signed-off-by: Tiktodz <dotkit@electrowizard.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,15 +6,17 @@ rm -rf build/make
 rm -rf device/asus
 rm -rf kernel/asus
 rm -rf vendor/asus
-rm -rf frameworks/base
+rm -rf lineage/scripts
+#rm -rf frameworks/base
 
 ## cloning
-git clone --depth=1 https://github.com/Tiktodz/android_frameworks_base frameworks/base
-git clone --depth=1 https://github.com/texascake/crdroid_build build/make
+#git clone --depth=1 https://github.com/Tiktodz/android_frameworks_base frameworks/base
+git clone --depth=1 https://github.com/Tiktodz/android_build build/make
 git clone --depth=1 https://github.com/Tiktodz/device_asus_X00TD-4.19 device/asus/X00TD
 git clone --depth=1 https://github.com/Tiktodz/device_asus_sdm660-common-4.19 device/asus/sdm660-common
-git clone --depth=1 --recurse-submodules https://github.com/Tiktodz/android_kernel_asus_sdm660 -b wip --single-branch kernel/asus/sdm660
+git clone --depth=1 --recurse-submodules https://github.com/Tiktodz/android_kernel_asus_sdm660 --single-branch kernel/asus/sdm660
 git clone --depth=1 https://github.com/Tiktodz/vendor_asus-4.19 vendor/asus
+git clone --depth=1 https://github.com/Tiktodz/scripts lineage/scripts
 
 ### Renaming
 sed -i 's/CONFIG_LOCALVERSION=.*/CONFIG_LOCALVERSION="-TOMTzy-969"/g' kernel/asus/sdm660/arch/arm64/configs/asus/X00TD_defconfig
@@ -22,14 +24,15 @@ sed -i 's/CONFIG_LOCALVERSION=.*/CONFIG_LOCALVERSION="-TOMTzy-969"/g' kernel/asu
 
 #### signing
 rm -rf vendor/lineage-priv/keys
-mkdir -p vendor/lineage-priv/keys
-cp -R lineage/scripts/lineage-priv-template/* vendor/lineage-priv/keys/
-cd vendor/lineage-priv/keys
+rm -rf vendor/custom-priv/keys
+mkdir -p vendor/custom-priv/keys
+cp -R lineage/scripts/lineage-priv-template/* vendor/custom-priv/keys/
+cd vendor/custom-priv/keys
 bash keys.sh
 cd -
 
 export BUILD_USERNAME=queen
-export BUILD_HOSTNAME=$DISTRO
+export BUILD_HOSTNAME=$(cat /etc/hostname)
 export TZ=Asia/Jakarta
 
 #####hanaqueen#####
